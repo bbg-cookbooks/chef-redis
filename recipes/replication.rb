f4ab2b79674ee6aef38a86970f31df8d423638cd
Chris Lundquist <chris.lundquist@bluebox.net>
2013-01-24 13:48:23 -0800

    style normalization

--- recipes/replication.rb
+++ recipes/replication.rb
@@ -1,8 +1,8 @@
-unless(node['redis']['replication']['tunnel']['enabled'])
-  node.default['redis']['config']['bind'] = node[:ipaddress]
+unless(node.redis.replication.tunnel.enabled)
+  node.default.redis.config.bind = node.ipaddress
 end
 
-if(node['redis']['replication']['redis_replication_role'] == 'slave')
+if(node.redis.replication.redis_replication_role == 'slave')
   master = discovery_search(
     'redis_replication_role:master',
     :environment_aware => true,
@@ -11,37 +11,37 @@ if(node['redis']['replication']['redis_replication_role'] == 'slave')
   )
 
   if(master)
-    if(node['redis']['replication']['tunnel']['enabled'])
+    if(node.redis.replication.tunnel.enabled)
       include_recipe 'stunnel'
 
       stunnel_connection 'redis' do
-        connect "#{master['ipaddress']}:#{master['redis']['replication']['tunnel']['accept_port']}"
-        accept "127.0.0.1:#{node['redis']['replication']['tunnel']['accept_port']}"
+        connect "#{master.ipaddress}:#{master.redis.replication.tunnel.accept_port}"
+        accept "127.0.0.1:#{node.redis.replication.tunnel.accept_port}"
         notifies :restart, 'service[stunnel]'
       end
 
-      node.normal['redis']['config']['slaveof'] = "127.0.0.1 #{node['redis']['replication']['tunnel']['accept_port']}"
+      node.normal.redis.config.slaveof = "127.0.0.1 #{node.redis.replication.tunnel.accept_port}"
     else
-      node.normal['redis']['config']['slaveof'] = "#{master['ipaddress']} #{master['redis']['config']['port']}"
+      node.normal.redis.config.slaveof = "#{master.ipaddress} #{master.redis.config.port}"
     end
   end
 else
-  if(node['redis']['replication']['tunnel']['enabled'])
+  if(node.redis.replication.tunnel.enabled)
     include_recipe 'stunnel::server'
 
-    node.normal['redis']['config']['bind'] = '127.0.0.1'
+    node.normal.redis.config.bind = '127.0.0.1'
 
     stunnel_connection 'redis' do
-      accept node['redis']['replication']['tunnel']['accept_port']
-      connect "#{node['redis']['config']['bind']}:#{node['redis']['config']['port']}"
+      accept node.redis.replication.tunnel.accept_port
+      connect "#{node.redis.config.bind}:#{node.redis.config.port}"
       notifies :restart, 'service[stunnel]'
     end
 
-    service "redis" do
+    service 'redis' do
       notifies :restart, 'service[stunnel]'
     end
   else
-    node.normal['redis']['config']['bind'] = node[:ipaddress]
+    node.normal.redis.config.bind = node.ipaddress
   end
 end
 
--- recipes/sentinel.rb
+++ recipes/sentinel.rb
@@ -1,21 +1,21 @@
-if(node['redis']['replication']['tunnel']['enabled'])
+if( node.redis.replication.tunnel.enabled)
   raise 'Sentinel is not currently supported with tunneled connections'
 end
 
 include_recipe 'redis::replication'
 
 basics = {
-  'down_after_milliseconds' => node['redis']['sentinel']['down_after'],
-  'failover_timeout' => node['redis']['sentinel']['failover_timeout'],
-  'can_failover' => node['redis']['sentinel']['can_failover'],
-  'parallel_syncs' => node['redis']['sentinel']['parallel_syncs']
+  'down_after_milliseconds' => node.redis.sentinel.down_after,
+  'failover_timeout' => node.redis.sentinel.failover_timeout,
+  'can_failover' => node.redis.sentinel.can_failover,
+  'parallel_syncs' => node.redis.sentinel.parallel_syncs
 }
 
-node.default['redis']['sentinel']['mon_config'][node.name] = {
-  :monitor => "#{node['redis']['config']['bind']} #{node['redis']['config']['port']} #{node['redis']['sentinel']['quorum']}"
+node.default.redis.sentinel.mon_config[node.name] = {
+  :monitor => "#{node.redis.config.bind} #{node.redis.config.port} #{node.redis.sentinel.quorum}"
 }.merge(basics)
 
-template File.join(node['redis']['conf_dir'], 'sentinel.conf') do
+template File.join(node.redis.conf_dir, 'sentinel.conf') do
   source 'sentinel.conf.erb'
   mode 0644
   notifies :restart, 'service[redis-sentinel]'
