295bcc61baa87384065fcd239c5e99ab6c0f1371
Sam Cooper <sam@chgworks.com>
2013-02-28 09:33:26 -0800

    [rm_vm_attrs] remove vm_* atts

--- providers/instance.rb
+++ providers/instance.rb
@@ -1,7 +1,6 @@
 def load_current_resource
   # Because these attributes are loaded lazily
   # we have to call each one explicitly
-  new_resource.vm_swap_file new_resource.vm_swap_file ||"/var/lib/redis/#{new_resource.name}.swap"
   new_resource.pidfile      new_resource.pidfile || "/var/run/redis/#{new_resource.name}.pid"
   new_resource.logfile      new_resource.logfile || "/var/log/redis/#{new_resource.name}.log"
   new_resource.dbfilename   new_resource.dbfilename || "#{new_resource.name}.rdb"
